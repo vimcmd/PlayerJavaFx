cf6c277e0af054dbff6896d710ccd84de6862b68
vimcmd <vim.cmd@gmail.com>
2016-03-27 13:20:14 +0500

    Initial commit

---
+++ src/sample/Player.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class Player extends BorderPane {
+
+    Media media;
+    MediaPlayer player;
+    MediaView view;
+    Pane mpane;
+
+    public Player(String filePath) {
+        media = new Media(filePath);
+        player = new MediaPlayer(media);
+        view = new MediaView(player);
+        mpane = new Pane();
+
+        mpane.getChildren().add(view);
+
+        setCenter(mpane);
+        player.play();
+    }
+}
